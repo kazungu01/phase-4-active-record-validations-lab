853916d4be64aa29fa5caba809214ecc5a6a9c7f
hassan kazungu <hassankazungu01@gmail.com>
2023-03-18 11:44:51 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+    # def index
+
+    # end
+
+    # def show
+    # end
+
+    # def create
+    # end
+
+    # def destroy
+    # end
+
 end
